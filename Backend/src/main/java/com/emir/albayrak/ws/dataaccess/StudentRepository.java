8467ae3abfd69e38f81d839ffecff784d8b1a050
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-01-26 22:11:57 +0300

    removed Parent objects

--- Backend/src/main/java/com/emir/albayrak/ws/dataaccess/StudentRepository.java
+++ Backend/src/main/java/com/emir/albayrak/ws/dataaccess/StudentRepository.java
@@ -1,15 +1,11 @@
 package com.emir.albayrak.ws.dataaccess;
 
-import com.emir.albayrak.ws.model.Parent;
 import com.emir.albayrak.ws.model.Student;
-import com.emir.albayrak.ws.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
-import utility.result.DataResult;
 
 import java.util.List;
 
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-    Parent findParentById(int studentId);
     Student findById(int id);
     Student findByNo(String no);
     List<Student> findAllByName(String name);
