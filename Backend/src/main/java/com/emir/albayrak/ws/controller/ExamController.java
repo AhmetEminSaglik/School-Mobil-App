47f6b6e01ea6f07991671dc6ffdf6879a1d3fd27
Ahmet Emin SAGLIK <ahmeteminsaglik@gmail.com>
2024-02-03 14:38:14 +0300

    Exam save and findAll Controller functions are added

--- Backend/src/main/java/com/emir/albayrak/ws/controller/ExamController.java
+++ Backend/src/main/java/com/emir/albayrak/ws/controller/ExamController.java
@@ -1,12 +1,17 @@
 package com.emir.albayrak.ws.controller;
 
 import com.emir.albayrak.ws.business.abstracts.model.ExamService;
+import com.emir.albayrak.ws.business.abstracts.model.StudentService;
+import com.emir.albayrak.ws.business.abstracts.model.TeacherService;
 import com.emir.albayrak.ws.model.Announcement;
 import com.emir.albayrak.ws.model.Exam;
+import com.emir.albayrak.ws.model.Student;
+import com.emir.albayrak.ws.model.Teacher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import utility.CustomLog;
 import utility.result.DataResult;
 import utility.result.SuccessDataResult;
 
@@ -16,18 +21,25 @@ import java.util.List;
 @RequestMapping("/exams/")
 public class ExamController {
     private ExamService service;
+    private CustomLog customLog = new CustomLog(getClass());
+
+    private TeacherService teacherService;
+    private StudentService studentService;
+
+    public ExamController(ExamService service, TeacherService teacherService, StudentService studentService) {
+        this.service = service;
+        this.teacherService = teacherService;
+        this.studentService = studentService;
+    }
 
     @Autowired
-    public ExamController(ExamService service) {
-        this.service = service;
-    }
 
     @GetMapping
     public ResponseEntity<DataResult<List<Exam>>> findAll() {
         List<Exam> list = service.findAll();
         String msg;
         if (!list.isEmpty()) {
-            msg = "Bütün sınavlar getirildi. ("+list.size()+" adet)";
+            msg = "Bütün sınavlar getirildi. (" + list.size() + " adet)";
         } else {
             msg = "Kayıtlı bir sınav bulunamadı";
         }
@@ -38,6 +50,12 @@ public class ExamController {
 
     @PostMapping
     public ResponseEntity<DataResult<Exam>> save(@RequestBody Exam exam) {
+        Teacher teacher = teacherService.findById(exam.getTeacher().getId());
+        Student student = studentService.findById(exam.getStudent().getId());
+        exam.setCourseName(teacher.getBranch());
+        exam.setTeacher(teacher);
+        exam.setStudent(student);
+        customLog.info("exam : " + exam);
         exam = service.save(exam);
         String msg = "Sınav kaydedildi.";
         DataResult<Exam> dataResult = new SuccessDataResult<>(exam, msg);
--- Backend/src/main/java/com/emir/albayrak/ws/model/Exam.java
+++ Backend/src/main/java/com/emir/albayrak/ws/model/Exam.java
@@ -17,15 +17,75 @@ public class Exam {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private int id;
-    @ManyToMany
-    private List<Teacher> teacherList;
-    @ManyToMany
-    private List<Student> studentList;
+    @OneToOne
+    private Teacher teacher;
+    @OneToOne
+    private Student student;
     @Column
-    private  String courseName;
+    private String courseName;
     @Column
     private int vizeResult;
     @Column
     private int finalResult;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(Teacher teacher) {
+        this.teacher = teacher;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public int getVizeResult() {
+        return vizeResult;
+    }
+
+    public void setVizeResult(int vizeResult) {
+        this.vizeResult = vizeResult;
+    }
+
+    public int getFinalResult() {
+        return finalResult;
+    }
+
+    public void setFinalResult(int finalResult) {
+        this.finalResult = finalResult;
+    }
+
+    @Override
+    public String toString() {
+        return "Exam{" +
+                "id=" + id +
+                ", teacher=" + teacher.getName() +
+                ", teacher branch=" + teacher.getBranch() +
+                ", student=" + student.getName() +
+                ", courseName='" + courseName + '\'' +
+                ", vizeResult=" + vizeResult +
+                ", finalResult=" + finalResult +
+                '}';
+    }
 }
