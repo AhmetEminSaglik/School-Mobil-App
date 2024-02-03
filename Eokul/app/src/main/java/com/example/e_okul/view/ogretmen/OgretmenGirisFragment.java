1c8ff47793666ca9fa9d908d12804a253568b232
Mustafa Karaman <karamanmustafa854@gmail.com>
2024-02-03 16:39:07 +0300

    öğretmen giriş

--- Eokul/app/src/main/java/com/example/e_okul/view/ogretmen/OgretmenGirisFragment.java
+++ Eokul/app/src/main/java/com/example/e_okul/view/ogretmen/OgretmenGirisFragment.java
@@ -8,12 +8,9 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.example.e_okul.R;
 import com.example.e_okul.databinding.FragmentOgretmenGirisBinding;
 import com.example.e_okul.model.LoginCredentials;
